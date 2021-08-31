690618fe5c889daeac73c47aca976c90506da4d4
Mohammed Hussian <mohussain237@gmail.com>
2021-08-31 12:05:37 +0100

    connecting to server and console logging the ENV_Port

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ import cors from "cors";
 
 const app = express();
 
+console.log(process.env.PORT.bold.underline)
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -26,12 +28,11 @@ app.use((err, req, res, next) => {
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
+  
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(
-  console.log(
-    `server is listning on ${process.env.NODE_ENV} mode on Port ${PORT}`
-      .yellow.bold
-  )
-);
+app.listen( process.env.PORT, ()=> { console.log(  `server is listning on ${process.env.NODE_ENV} mode on Port ${PORT}`
+.yellow.bold)}
+
+)
