b404abe4c46e83013d966e6620aee8f98ecfd16f
Mohammed Hussian <mohussain237@gmail.com>
2021-09-11 08:47:07 +0100

    adding route with hardcoded function

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,33 +1,13 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
+import {getProducts,getProductsById} from '../controllers/productController.js'
+
+
+
 const router = express.Router();
-import Product from "../models/productModel.js";
 
-// @desc  Fetch all Produtcs
-// @route GET/api/products
-// @access  public
 
-router.get("/",asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductsById)
 
-// @desc  Fetch single  Produtc
-// @route GET/api/products/:id
-// @access  public
-
-router.get("/:id", asyncHandler(async (req, res) => {
-    console.log("reach here")
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-  })
-);
 
 export default router;
